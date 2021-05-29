8b30638a6814c0d8f622bbbb827ea2f7d0e2bf17
Eroica <seba79@mailbox.org>
2021-05-29 16:09:11 +0200

    javafx: Update dependencies

--- javafx/build.gradle.kts
+++ javafx/build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.5.10"
     id("org.openjfx.javafxplugin") version "0.0.9"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("org.beryx.runtime") version "1.12.1"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.beryx.runtime") version "1.12.5"
 }
 
 group = "app.paletti.javafx"
@@ -16,17 +16,17 @@ repositories {
 }
 
 javafx {
-    version = "14"
+    version = "16"
     modules("javafx.controls", "javafx.fxml", "javafx.swing")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("io.reactivex.rxjava2:rxjava:2.2.20")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("io.reactivex.rxjava2:rxjavafx:2.2.2")
     implementation("org.xerial:sqlite-jdbc:3.34.0")
-    implementation("net.harawata:appdirs:1.2.0")
+    implementation("net.harawata:appdirs:1.2.1")
     implementation("org.slf4j:slf4j-simple:1.7.30")
 }
 
