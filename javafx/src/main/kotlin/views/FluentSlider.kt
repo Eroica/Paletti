e66c11dde670116e7e8a9f0b725da8f9ae5555d9
Eroica <Eroica@users.noreply.github.com>
2021-07-25 19:02:25 +0200

    javafx: Update to WinUI 3 styles

---
+++ javafx/src/main/kotlin/views/FluentSlider.kt
@@ -0,0 +1,51 @@
+package views
+
+import javafx.animation.Interpolator
+import javafx.animation.KeyFrame
+import javafx.animation.KeyValue
+import javafx.animation.Timeline
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.control.Skin
+import javafx.scene.control.Slider
+import javafx.scene.control.skin.SliderSkin
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.StackPane
+import javafx.util.Duration
+
+class FluentSlider : Slider() {
+    override fun createDefaultSkin(): Skin<*> {
+        return FluentSliderSkin(this)
+    }
+}
+
+class FluentSliderSkin(slider: FluentSlider) : SliderSkin(slider) {
+    private val thumb = skinnable.lookup(".thumb") as StackPane
+
+    init {
+        val radiusProperty = SimpleIntegerProperty()
+        thumb.styleProperty().bind(SimpleStringProperty("-fx-background-insets: -8px, -6px, -5px, ")
+            .concat(radiusProperty)
+            .concat("px;"))
+        val scaleMoveIn = Timeline(KeyFrame(Duration.millis(0.0), KeyValue(radiusProperty, 0)),
+            KeyFrame(Duration.millis(100.0), KeyValue(radiusProperty, -2, Interpolator.EASE_IN)))
+        val scaleMoveOut = Timeline(KeyFrame(Duration.millis(0.0), KeyValue(radiusProperty, -2)),
+            KeyFrame(Duration.millis(100.0), KeyValue(radiusProperty, 0, Interpolator.EASE_OUT)))
+        val scalePressed = Timeline(KeyFrame(Duration.millis(0.0), KeyValue(radiusProperty, 0)),
+            KeyFrame(Duration.millis(60.0), KeyValue(radiusProperty, 1, Interpolator.EASE_IN)))
+        val scaleReleased = Timeline(KeyFrame(Duration.millis(0.0), KeyValue(radiusProperty, 0)),
+            KeyFrame(Duration.millis(60.0), KeyValue(radiusProperty, -2, Interpolator.EASE_IN)))
+        thumb.addEventFilter(MouseEvent.MOUSE_ENTERED) {
+            scaleMoveIn.play()
+        }
+        thumb.addEventFilter(MouseEvent.MOUSE_EXITED) {
+            scaleMoveOut.play()
+        }
+        thumb.addEventFilter(MouseEvent.MOUSE_PRESSED) {
+            scalePressed.play()
+        }
+        thumb.addEventFilter(MouseEvent.MOUSE_RELEASED) {
+            scaleReleased.play()
+        }
+    }
+}
