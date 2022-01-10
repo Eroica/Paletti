f1715fcad26a6e22c81fc92b0b9701f5be1a08f0
Eroica <Eroica@users.noreply.github.com>
2022-01-11 00:19:00 +0100

    javafx: Use native Windows 11 window frame

--- javafx/src/main/kotlin/app/paletti/lib/Leptonica.kt
+++ javafx/src/main/kotlin/app/paletti/lib/Leptonica.kt
@@ -3,10 +3,6 @@ package app.paletti.lib
 object Leptonica {
     const val OK = 0
 
-    init {
-        System.loadLibrary("Paletti")
-    }
-
     external fun posterize(count: Int, isBlackWhite: Boolean, paths: Array<String>): Int
 
     external fun posterize2(imageId: Int, databasePath: String): Int
