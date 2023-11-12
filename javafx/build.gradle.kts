4943f09a0a8055866b9e08422d9df4a8cd4aed36
Eroica <seba@mails.taipei>
2023-11-12 17:38:58 +0100

    javafx: Update dependencies

--- javafx/build.gradle.kts
+++ javafx/build.gradle.kts
@@ -1,44 +1,44 @@
 plugins {
     application
-    kotlin("jvm") version "1.8.20"
-    id("org.openjfx.javafxplugin") version "0.0.13"
+    kotlin("jvm") version "1.9.20"
+    id("org.openjfx.javafxplugin") version "0.1.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.beryx.runtime") version "1.12.7"
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.github.ben-manes.versions") version "0.49.0"
     id("com.jaredsburrows.license")
-    id("com.github.gmazzo.buildconfig") version "3.1.0"
+    id("com.github.gmazzo.buildconfig") version "4.1.2"
 }
 
 group = "app.paletti.javafx"
-version = "2023.04"
+version = "2023.11"
 
 repositories {
     mavenCentral()
 }
 
 javafx {
-    version = "20"
+    version = "21"
     modules("javafx.controls", "javafx.fxml", "javafx.swing")
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.6.4")
-    implementation("org.xerial:sqlite-jdbc:3.41.2.1")
-    implementation("net.harawata:appdirs:1.2.1")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.7.3")
+    implementation("org.xerial:sqlite-jdbc:3.43.2.2")
+    implementation("net.harawata:appdirs:1.2.2")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks {
     compileJava {
-        targetCompatibility = JavaVersion.VERSION_19.toString()
+        targetCompatibility = JavaVersion.VERSION_20.toString()
     }
     compileKotlin {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_19.toString()
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_20.toString()
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_19.toString()
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_20.toString()
     }
     jpackage {
         finalizedBy("copyDlls")
@@ -57,8 +57,8 @@ buildConfig {
 
     buildConfigField("String", "APP_NAME", "\"${project.name}\"")
     buildConfigField("String", "APP_VERSION", "\"${project.version}\"")
-    buildConfigField("String", "APP_COPYRIGHT", "\"Copyright © 2023\"")
-    buildConfigField("String", "APP_LICENSE", "\"${project.name} (c) 2023 Eroica\"")
+    buildConfigField("String", "APP_COPYRIGHT", "\"Copyright © 2022-2023\"")
+    buildConfigField("String", "APP_LICENSE", "\"${project.name} (c) 2022-2023 Eroica\"")
 }
 
 application {
@@ -75,7 +75,7 @@ runtime {
         skipInstaller = true
         imageOptions = listOf(
             "--icon", "src/main/resources/Paletti.ico",
-            "--copyright", "2023",
+            "--copyright", "2022-2023",
             "--vendor", "GROUNDCTRL"
         )
     }
