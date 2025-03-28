32573a26d532181d3f27d5c03b929c291b6cef3c
Eroica <Eroica@noreply.users.github.com>
2025-03-28 09:39:26 +0800

    javafx: Update dependencies

--- javafx/build.gradle.kts
+++ javafx/build.gradle.kts
@@ -1,45 +1,38 @@
 plugins {
     application
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.1.20"
     id("org.openjfx.javafxplugin") version "0.1.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.beryx.runtime") version "1.12.7"
+    id("com.dua3.gradle.runtime") version "1.13.1-patch-1"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("com.jaredsburrows.license")
     id("com.github.gmazzo.buildconfig") version "5.3.5"
 }
 
 group = "app.paletti.javafx"
-version = "2024.04"
+version = "2024.05"
 
 repositories {
     mavenCentral()
 }
 
 javafx {
-    version = "22"
+    version = "24"
     modules("javafx.controls", "javafx.fxml", "javafx.swing")
 }
 
+kotlin {
+    jvmToolchain(22)
+}
+
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.7.3")
-    implementation("org.xerial:sqlite-jdbc:3.45.2.0")
-    implementation("net.harawata:appdirs:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.10.1")
+    implementation("org.xerial:sqlite-jdbc:3.49.1.0")
+    implementation("net.harawata:appdirs:1.4.0")
     implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks {
-    compileJava {
-        targetCompatibility = JavaVersion.VERSION_20.toString()
-    }
-    compileKotlin {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_20.toString()
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_20.toString()
-    }
     jpackage {
         finalizedBy("copyDlls")
     }
@@ -57,8 +50,8 @@ buildConfig {
 
     buildConfigField("String", "APP_NAME", "\"${project.name}\"")
     buildConfigField("String", "APP_VERSION", "\"${project.version}\"")
-    buildConfigField("String", "APP_COPYRIGHT", "\"Copyright © 2022-2024\"")
-    buildConfigField("String", "APP_LICENSE", "\"${project.name} (c) 2022-2024 Eroica\"")
+    buildConfigField("String", "APP_COPYRIGHT", "\"Copyright © 2022-2025\"")
+    buildConfigField("String", "APP_LICENSE", "\"${project.name} (c) 2022-2025 Eroica\"")
 }
 
 application {
