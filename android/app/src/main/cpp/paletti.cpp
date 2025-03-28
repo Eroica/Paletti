5e7d7b0f1ed526a69f95f64776a089d2adc67345
Eroica <Eroica@noreply.users.github.com>
2025-03-29 07:28:38 +0800

    android: Add better error handling to C++ source

--- android/app/src/main/cpp/paletti.cpp
+++ android/app/src/main/cpp/paletti.cpp
@@ -1,75 +1,96 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include <android/log.h>
 #include <jni.h>
 
 #include "allheaders.h"
 
-#define  LOG_TAG    "Paletti"
-
-#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
-#define APPNAME "Paletti"
+#define LOG_TAG "Paletti"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 extern "C" JNIEXPORT int JNICALL
 Java_app_paletti_lib_Leptonica_posterize(
-	JNIEnv * env,
+	JNIEnv *env,
 	jobject /* this */,
 	jint max_count,
 	jboolean is_black_white,
 	jobjectArray file_paths) {
-	std::string native_paths[3];
 	int len = env->GetArrayLength(file_paths);
+	std::vector<std::string> native_paths;
+	native_paths.reserve(len);
+
 	for (int i = 0; i < len; i++) {
 		jstring jstr = (jstring)(env->GetObjectArrayElement(file_paths, i));
-		const jsize strLen = env->GetStringUTFLength(jstr);
-		const char* charBuffer = env->GetStringUTFChars(jstr, (jboolean*)0);
-		std::string str(charBuffer, strLen);
-		native_paths[i] = str;
+		if (!jstr) {
+		    continue;
+		}
+
+		const char* charBuffer = env->GetStringUTFChars(jstr, nullptr);
+		native_paths.emplace_back(charBuffer);
 		env->ReleaseStringUTFChars(jstr, charBuffer);
 		env->DeleteLocalRef(jstr);
 	}
 
-	PIX* pixs = pixRead(native_paths[0].c_str());
-	if (pixs == nullptr) {
+	if (native_paths.size() < 3) {
+		LOGE("Error: Expected at least 3 file paths, got %d", len);
 		return -1;
 	}
 
-	PIX* pixc;
+	PIX *pixs = pixRead(native_paths[0].c_str());
+	if (!pixs) {
+		LOGE("Error: Failed to read input image %s", native_paths[0].c_str());
+		return -1;
+	}
+
+	PIX *pixc = nullptr;
 	if (is_black_white) {
-		PIX* tmp = pixConvertRGBToLuminance(pixs);
-		PIX* tmp2 = pixConvert8To32(tmp);
+		PIX *tmp = pixConvertRGBToLuminance(pixs);
+		PIX *tmp2 = pixConvert8To32(tmp);
 		pixc = pixMedianCutQuantGeneral(tmp2, 0, 8, (int)max_count, 0, 0, 0);
+
+		if (!pixc) {
+			LOGE("Error: pixMedianCutQuantGeneral failed");
+			pixDestroy(&tmp2);
+			pixDestroy(&tmp);
+			pixDestroy(&pixs);
+			return -1;
+		}
+
 		pixDestroy(&tmp2);
 		pixDestroy(&tmp);
-	}
-	else {
+	} else {
 		pixc = pixMedianCutQuantGeneral(pixs, 0, 8, (int)max_count, 0, 0, 0);
-	}
-	if (pixc == nullptr) {
-		pixDestroy(&pixs);
-		return -1;
+		if (!pixc) {
+			LOGE("Error: pixMedianCutQuantGeneral failed");
+			pixDestroy(&pixs);
+			return -1;
+		}
 	}
 
-	PIXCMAP* colormap = pixGetColormap(pixc);
+	PIXCMAP *colormap = pixGetColormap(pixc);
 	int count = pixcmapGetCount(colormap);
 
-	std::ofstream colors;
-	colors.open(native_paths[2].c_str());
-	for (int i = 0; i < count; i++) {
-		int r, g, b;
-		pixcmapGetColor(colormap, i, &r, &g, &b);
-		if (is_black_white && (r != g || r != b)) {
-			continue;
+	std::ofstream colors(native_paths[2]);
+	if (!colors.is_open()) {
+		LOGE("Error: Failed to open color output file %s", native_paths[2].c_str());
+	} else {
+		for (int i = 0; i < count; i++) {
+			int r, g, b;
+			pixcmapGetColor(colormap, i, &r, &g, &b);
+			if (is_black_white && (r != g || r != b)) continue;
+			colors << r << "," << g << "," << b << std::endl;
 		}
-		colors << r << "," << g << "," << b << std::endl;
+		colors.close();
 	}
-	colors.close();
 
-	pixWrite(native_paths[1].c_str(), pixc, 1);
+	if (pixWrite(native_paths[1].c_str(), pixc, 1) != 0) {
+		LOGE("Error: Failed to write image file %s", native_paths[1].c_str());
+	}
+
 	pixDestroy(&pixc);
 	pixDestroy(&pixs);
 	return 0;
